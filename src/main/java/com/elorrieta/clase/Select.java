2e169cd20fa4750c30c5115337953fa098415e3f
Javierbdp <javierbaz99@gmail.com>
2022-03-28 13:52:49 +0200

    Select movido a nueva clase

--- src/main/java/com/elorrieta/clase/Select.java
+++ src/main/java/com/elorrieta/clase/Select.java
@@ -1,5 +1,42 @@
 package com.elorrieta.clase;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Select {
 
+	
+	public void listar() {
+
+		String sql = "SELECT id_alumno, nombre, email FROM clase.alumno ORDER BY id_alumno ASC;";
+
+		try (Connection con = Conexion.getConnection();
+				PreparedStatement pst = con.prepareStatement(sql);
+				ResultSet rs = pst.executeQuery();
+
+		) {
+
+			System.out.println("-------------------------------------------------------");
+			System.out.println(" ID            nombre            email");
+			System.out.println("-------------------------------------------------------");
+			while (rs.next()) {
+
+				int id = rs.getInt("id_alumno");
+				String nombre = rs.getString("nombre");
+				String email = rs.getString("email");
+				System.out.printf(" %-4s %-25s %s \n", id, nombre, email);
+
+			} // while
+
+			System.out.println("---------------------- TOTAL X Alumnos -------------------");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}// listar
+
+	
+	
 }
