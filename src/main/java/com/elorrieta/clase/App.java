cd9a768122118b1d11d7dacb7ac0b830da9b30b0
Javierbdp <javierbaz99@gmail.com>
2022-03-29 11:07:02 +0200

    Listar por acabar, Main implementado y App bien estructurada

--- src/main/java/com/elorrieta/clase/App.java
+++ src/main/java/com/elorrieta/clase/App.java
@@ -13,11 +13,6 @@ import java.util.Scanner;
 public class App {
 
 	// variables globales para poder usar en todos los metodos de esta Clase
-	private static final int OPCION_LISTAR = 1;
-	private static final int OPCION_INSERTAR = 2;
-	private static final int OPCION_MODIFICAR = 3;
-	private static final int OPCION_ELIMINAR = 4;
-	private static final int OPCION_SALIR = 0;
 
 	private static final String SQL_INSERTALUMNO = "INSERT INTO alumno (nombre, email) VALUES (?,?)";
 
@@ -25,37 +20,7 @@ public class App {
 
 	private static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
 
-		System.out.println("Comenzamos");
-		boolean flag = true;
-
-		do {
-
-			opcion = menu();
-
-			switch (opcion) {
-			case OPCION_LISTAR:
-				new Select().listar();
-				break;
-
-			case OPCION_INSERTAR:
-				insertar();
-				break;
-
-			case OPCION_SALIR:
-				flag = false;
-				break;
-
-			default:
-				break;
-			}
-
-		} while (flag);
-
-		System.out.println("Terminamos");
-
-	}// main
 
 	/**
 	 * Pide por pantalla los datos de un alumno y lo inserta en la bbdd
@@ -102,26 +67,67 @@ public class App {
 		System.out.println("----------------------------------------------------");
 		System.out.println(" 1 - Listar Alumnos");
 		System.out.println(" 2 - Insertar Nuevo Alumno");
-		System.out.println(" 3 - sdasdasdasd");
-		System.out.println(" 4 - asdasdasdasd ");
+		System.out.println(" 3 - Modificar Alumno");
+		System.out.println(" 4 - Borrar Alumno ");
 		System.out.println("----------------------------------------------------");
 		System.out.println(" 0 - Salir");
 		System.out.println("----------------------------------------------------");
 
-		do {
+		
 			try {
 				op = Integer.parseInt(sc.nextLine().trim());
-				if (opcion < 0 && opcion > 4) {
-					error = true;
-					System.out.println("Debe introducir un numero del 0 al 4");
-				}
+				
 			} catch (Exception e) {
-				error = true;
 				System.out.println("Error en la introduccion de opcion, vuelve a introducir la opcion");
 				e.printStackTrace();
 			}
-		} while (error);
+		
 		return op;
 	}
+	
+	
+	private void leerOpcionMenu(int opcion) {
+	
+		switch (opcion) {
+		case 0: System.out.println("¡¡¡¡Que tennga un buen dia!!!!");
+			System.exit(opcion);
+			break;
+		case 1:
+			new Select().listar();
+			break;
+
+		case 2:
+			insertar();
+			break;
+
+		case 3:System.out.println("Sin terminar!!!!!!!!!!!");
+			//new Update().modificar();
+			break;
+			
+		case 4: System.out.println("Sin terminar!!!!!!!!!!!");
+			//new Delete().borrarAlumno();
+			break;
+		default: System.out.println("Opcion incorrecta, vuelva a seleccionar numero");
+			opcion = 99;
+		}
+	}	
+	
+	public void empezarPrograma() {
+		int opcion=0;
+		
+		do {
+			opcion=menu();
+			leerOpcionMenu(opcion);
+		}while(opcion !=0);
+	}
+
+
+		
+		
+		
+	//}
+	
+	
+	
 
 }// App
---
+++ src/main/java/com/elorrieta/clase/Main.java
@@ -0,0 +1,12 @@
+package com.elorrieta.clase;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new App().empezarPrograma();
+
+	}
+
+}
