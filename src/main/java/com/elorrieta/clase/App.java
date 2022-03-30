9917c31ff1f50216efc1a6e5d7d999dbafd24ee8
Javierbdp <javierbaz99@gmail.com>
2022-03-30 11:42:10 +0200

    Login hecho

--- src/main/java/com/elorrieta/clase/App.java
+++ src/main/java/com/elorrieta/clase/App.java
@@ -1,26 +1,43 @@
 package com.elorrieta.clase;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 /**
  * App para hacer un CRUD completo para la bbdd de clase.sql
  * 
- * @author infauraga
+ * @author infauraga, IbonJG, Javierbdp
  *
  */
 public class App {
 
-	private static int opcion = 0; // opcion seleccionada por el usuario
-
 	public static Scanner sc = new Scanner(System.in);
 
+	/**
+	 * Login de usuario,para poder entrar introducir nombre de usuario "admin" y
+	 * password "admin"
+	 */
+	private static void logueo() {
 
+		String nombre = "";
+		String pass = "";
+		boolean entrar = false;
 
+		do {
+			System.out.println("Introduce el nombre del usuario");
+			nombre = sc.nextLine();
+			System.out.println("Introduce el password");
+			pass = sc.nextLine();
 
+			if (nombre.equalsIgnoreCase("admin") && pass.equals("admin")) {
+				System.out.println("¡¡¡Bienvenido!!!\n");
+				entrar = true;
+			} else {
+				System.out.println("Error con el nombre de usuario o password, por favor vuelva a intentarlo");
+				entrar = false;
+			}
+		} while (!entrar);
+	}
 
-	
 	/**
 	 * Pinta por pantalla el menu de la App y pide al usuario que seleccione una
 	 * opcion
@@ -43,20 +60,19 @@ public class App {
 
 		try {
 			op = Integer.parseInt(sc.nextLine().trim());
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.getMessage();
-			op=99;
+			op = 99;
 		}
-		
+
 		return op;
 	}
 
-	
-	
 	private static void leerOpcionMenu(int opcion) {
-	
+
 		switch (opcion) {
-		case 0: System.out.println("¡¡¡¡Que tennga un buen dia!!!!");
+		case 0:
+			System.out.println("¡¡¡¡Que tennga un buen dia!!!!");
 			sc.close();
 			break;
 		case 1:
@@ -70,33 +86,31 @@ public class App {
 		case 3:
 			new Update().modificar(sc);
 			break;
-			
-		case 4: 
+
+		case 4:
 			new Delete().borrarAlumno(sc);
 			break;
-		default: System.out.println("Opcion incorrecta, vuelva a seleccionar numero");
+		default:
+			System.out.println("Opcion incorrecta, vuelva a seleccionar numero");
 			opcion = 99;
 		}
-	}	
-	
-	public static void main(String[] args) {
-		int opcion=0;
-		
-		do {
-			opcion=menu();
-			leerOpcionMenu(opcion);
-		}while(opcion !=0);
 	}
 
+	/**
+	 * Main del programa, en este metodo se ejecuta todo el rato el programa hasta
+	 * que el usuario introduzca 0
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int opcion = 0;
 
-		
-		
-		
+		logueo();
 
-	
-	
-	
-
-
+		do {
+			opcion = menu();
+			leerOpcionMenu(opcion);
+		} while (opcion != 0);
+	}
 
 }// App
